652837affc430c43bb9bdf2b93e7c5d987ba8581
HowieHz <94725606+HowieHz@users.noreply.github.com>
2022-07-16 09:29:05 +0800

    hpyc_cli v1.0.1
    代码风格优化

--- hpyc_cli/__main__.py
+++ hpyc_cli/__main__.py
@@ -1,9 +1,8 @@
 from hpyc_core import Core
-from pprint import pprint
 import os
 
-VERSION = "V1.0.0"
-yes = ("y", "Y", "YES", "Yes", "yes")
+VERSION = "V1.0.1"
+yes = ("y", "Y", "YES", "Yes", "yes", "true", "True", "是", "确认")
 instance_core = Core()
 message_queue = instance_core.getMessageQueue()
 
@@ -16,39 +15,36 @@ if __name__ == "__main__":
 输出目录: {instance_core.getOutputDirPath()}
 获取到的插件选项名: """
     )
-    pprint(tuple(instance_core.getPluginsOptionToId().keys()))
+    [print(id) for id in instance_core.getPluginsOptionToId().keys()]
     while True:
-        option = input("输入你想使用的插件的选项名, 如想退出输入exit\n:")
-        if option == "exit":
-            print(f"感谢您的使用, hpyc_cli version:{VERSION}")
+        if (option := input("\n输入你想使用的插件的选项名, 如想退出输入exit\n:")) == "exit":
+            input(f"感谢您的使用, hpyc_cli version:{VERSION}\n按下回车键退出程序")
             break
 
-        plugin_id = instance_core.getPluginIdFromOption(option)
+        print(
+            f"使用提示：\n{instance_core.getPluginMetadata((plugin_id := instance_core.getPluginIdFromOption(option)))['help']}\n"
+        )
 
-        print(f"使用提示：\n{instance_core.getPluginMetadata(plugin_id)['help']}")
-
-        if not input("如想使用此插件请输入 y 否则输入 n\n:") in yes:
+        if not (input("如想使用此插件请输入 y 否则输入 n\n:") in yes):
             continue
-        rev_input = input(r"请输入待处理数据(如需换行请输入 \n ):")
 
-        if input("如想将结果保存至文件请输入 y 否则输入 n\n:") in yes:
-            instance_core.eventStartCalculate(
-                plugin_id=plugin_id, input_data=rev_input, mode="Save"
-            )
-        else:
-            instance_core.eventStartCalculate(
-                plugin_id=plugin_id, input_data=rev_input, mode="Return"
-            )
+        instance_core.eventStartCalculate(
+            plugin_id=plugin_id,
+            input_data=input("请输入待处理数据(如需换行请输入 \\n )\n:"),
+            mode=("Save" if input("如想将结果保存至文件请输入 y 否则输入 n\n:") in yes else "Return"),
+        )
+
         while True:  # 消息处理
             rev = message_queue.get()
-            if rev[0] == "OUTPUT":
-                print(rev[1])
-            elif rev[0] == "ERROR":
-                print(f"发生了{rev[1]}\n详细错误:{rev[2]}")
-                break
-            elif rev[0] == "MESSAGE":
-                if rev[1] == "CalculationProgramIsRunning":
-                    print("正在计算, 请耐心等待")
-                elif rev[1] == "CalculationProgramIsFinished":
-                    print(f"计算完毕, 花费了{rev[2]}ns")
+            match rev[0]:
+                case "OUTPUT":
+                    print(rev[1])
+                case "ERROR":
+                    print(f"发生了{rev[1]}\n详细错误:{rev[2]}")
                     break
+                case "MESSAGE":
+                    if rev[1] == "CalculationProgramIsRunning":
+                        print("正在计算, 请耐心等待")
+                    elif rev[1] == "CalculationProgramIsFinished":
+                        print(f"计算完毕, 花费了{rev[2]}ns")
+                        break
